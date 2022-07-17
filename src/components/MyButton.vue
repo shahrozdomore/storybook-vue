f10863821f3d4bed8e73cc734621137d2fec40ef
Shahroz Ali Khan <shahrozdomore@gmail.com>
2022-07-18 02:01:58 +0500

    initial commit

---
+++ src/components/MyButton.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { computed, ref } from "vue";
+
+const { label, bgColor } = withDefaults(
+  defineProps<{ label: string; bgColor?: string }>(),
+  {
+    bgColor: "#000",
+  }
+);
+
+const style = computed(() => "background: " + bgColor);
+
+//  style: computed(() => ({
+//         backgroundColor: bgColor,
+//       })),
+</script>
+
+<template>
+  <div>
+    <button
+      type="button"
+      class="bg-red-500 text-white p-2 rounded-lg"
+      :style="style"
+    >
+      {{ label }}
+    </button>
+  </div>
+</template>
---
+++ src/stories/MyButton.stories.js
@@ -0,0 +1,26 @@
+import MyButton from "../components/MyButton.vue";
+
+export default {
+  title: "UI/MyButton",
+  component: MyButton,
+};
+
+const Template = (args) => ({
+  components: { MyButton },
+  setup() {
+    return { args };
+  },
+  template: '<MyButton v-bind="args"/>',
+});
+
+export const Primary = Template.bind({});
+Primary.args = {
+  label: "Hello",
+  bgColor: "green",
+};
+
+export const Secondary = Template.bind({});
+Secondary.args = {
+  label: "Bye",
+  bgColor: "red",
+};
